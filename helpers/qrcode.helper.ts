4d07d2d3b4bbe20510b10e173b02a6026a13110f
daniel-dunsin <adejaredaniel12@gmail.com>
2024-03-10 06:36:33 +0100

    fix: uploaded qrcode to cloud cuz ase64 isn't working for gmail

--- helpers/qrcode.helper.ts
+++ helpers/qrcode.helper.ts
@@ -1,11 +1,14 @@
 import { GenerateQrCode } from "../schema/dto/qrcode.dto";
 import QrCode from "qrcode";
 import ServiceException from "../schema/exceptions/service.exception";
+import { uploadResource } from "./upload.helper";
 
 export async function generateQrCode(data: GenerateQrCode): Promise<string> {
   try {
-    const url = await QrCode.toDataURL(JSON.stringify(data), { errorCorrectionLevel: "L" });
-    console.log(url);
+    const qrCodeUrl = await QrCode.toDataURL(JSON.stringify(data), { errorCorrectionLevel: "L" });
+
+    const { url } = await uploadResource(qrCodeUrl, { folder: "qr_codes" });
+
     return url;
   } catch (error) {
     throw new ServiceException(500, "unable to generate qrcode");
